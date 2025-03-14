91b344d8985eb058a7e9639c3fa532abbca12472
david1derevanko <dereviankodavyd@gmail.com>
2025-03-14 22:15:15 +0200

    init22

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-print ("Hello world")
+def get_grade(score):
+    if 0 <= score <= 49:
+        return "незадовільно"
+    elif 50 <= score <= 69:
+        return "задовільно"
+    elif 70 <= score <= 89:
+        return "добре"
+    elif 90 <= score <= 100:
+        return "відмінно"
+    else:
+        return "Некоректне значення балів"
+
+try:
+    score = int(input("Введіть кількість отриманих балів: "))
+    print(f"Ваша оцінка: {get_grade(score)}")
+except ValueError:
+    print("Будь ласка, введіть ціле число.")
